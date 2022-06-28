e7ce84f242a8d147b30bd64fffb8bb8139c9cc66
DUBV <dubydu97@gmail.com>
2022-06-28 14:47:57 +0700

    update: register sliver

--- lib/di/service _locator.dart
+++ lib/di/service _locator.dart
@@ -1,7 +1,10 @@
 import 'package:fluttour/config/app_config.dart';
 import 'package:fluttour/data/datasource/home/home_datasource.dart';
+import 'package:fluttour/data/datasource/sliver/sliver_datasource.dart';
 import 'package:fluttour/data/repository/home/home_repository.dart';
+import 'package:fluttour/data/repository/sliver/sliver_repository.dart';
 import 'package:fluttour/domain/usecase/home/home_usecase.dart';
+import 'package:fluttour/domain/usecase/sliver/sliver_usecase.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt get = GetIt.instance;
@@ -13,11 +16,14 @@ class ServiceLocator {
 
     /// Use case
     get.registerFactory(() => HomeUseCase(repository: get()));
+    get.registerFactory(() => SliverUseCase(repository: get()));
     
     /// Repository
     get.registerFactory(() => HomeRepository(dataSource: get()));
+    get.registerFactory(() => SliverRepository(dataSource: get()));
     
     /// Data source
     get.registerFactory(() => HomeDataSource(baseDomain: baseDomain));
+    get.registerFactory(() => SliverDataSource(baseDomain: baseDomain));
   }
 }
