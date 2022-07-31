a24c06b935c027e744bc79bbf10c3b911d4e535b
DU <dubydu97@gmail.com>
2022-07-31 13:14:29 +0700

    chore: lint code

--- lib/di/client_module.dart
+++ lib/di/client_module.dart
@@ -5,7 +5,7 @@ import 'package:fluttour/data/api/graphql_client/graphql_client_type.dart';
 import 'package:fluttour/di/config_module.dart';
 
 mixin ClientModule on ConfigModule {
-  /// API Client
+  /// API/REST Client
   APIClientType get apiClient {
     return APIClient.apiClient(
         baseDomain: appConfig.baseDomain,
