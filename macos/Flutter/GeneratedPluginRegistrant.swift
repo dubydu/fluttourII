5888d1b413dbc93ebc0045b3b752ab45467ac498
Du Bui <dub@tracersystems.co.uk>
2022-11-08 14:01:15 +0700

    feat: cocoapods installation

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_plus_macos
+import connectivity_plus
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
